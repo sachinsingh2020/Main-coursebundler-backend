a3ccfa2c388d93e6ddaae261330c15c90f60ae42
sachinsingh2020 <sachinthegreat1001@gmail.com>
2023-03-05 16:08:11 +0530

    eleven

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,37 +1,37 @@
-import jwt from "jsonwebtoken";
-import ErrorHandler from "../utils/errorHandler.js";
-import { catchAsyncError } from "./catchAsyncError.js";
-import { User } from "../models/User.js";
+import jwt from 'jsonwebtoken';
+import { catchAsyncError } from './CatchAsyncError.js';
+import { User } from '../models/User.js';
+import ErrorHandler from '../utils/errorHandler.js';
+
 
 export const isAuthenticated = catchAsyncError(async (req, res, next) => {
-  const { token } = req.cookies;
+    const { token } = req.cookies;
+    
 
-  if (!token) return next(new ErrorHandler("Not Logged In", 401));
+    if (!token) {
+        return next(new ErrorHandler('Login First To Access This Resource', 401));
+    }
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-  req.user = await User.findById(decoded._id);
+    req.user = await User.findById(decoded._id);
 
-  next();
-});
+    next();
+})
 
-export const authorizeSubscribers = (req, res, next) => {
-  if (req.user.subscription.status !== "active" && req.user.role !== "admin")
-    return next(
-      new ErrorHandler(`Only Subscribers can acces this resource`, 403)
-    );
 
-  next();
-};
 
 export const authorizeAdmin = (req, res, next) => {
-  if (req.user.role !== "admin")
-    return next(
-      new ErrorHandler(
-        `${req.user.role} is not allowed to access this resource`,
-        403
-      )
-    );
+    if (req.user.role != "admin")
+        return next(new ErrorHandler(`${req.user.role} is not allowed to access this resource`, 403));
 
-  next();
+    next();
+};
+
+export const authorizeSubscribers = (req, res, next) => {
+    if (req.user.subscription.status !== "active" && req.user.role !== "admin")
+        return next(
+            new ErrorHandler(`Only Subscribers can acces this resource`, 403)
+        );
+    next();
 };
