e93319bddada607ffe54dc7f656f9ef3202d3d2d
sachinsingh2020 <sachinthegreat1001@gmail.com>
2023-03-05 15:54:47 +0530

    eight

--- models/User.js
+++ models/User.js
@@ -5,91 +5,94 @@ import bcrypt from "bcrypt";
 import crypto from "crypto";
 
 const schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please enter your name"],
-  },
-  email: {
-    type: String,
-    required: [true, "Please enter your email"],
-    unique: true,
-    validate: validator.isEmail,
-  },
-
-  password: {
-    type: String,
-    required: [true, "Please enter your password"],
-    minLength: [6, "Password must be at least 6 characters"],
-    select: false,
-  },
-  role: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user",
-  },
-
-  subscription: {
-    id: String,
-    status: String,
-  },
-
-  avatar: {
-    public_id: {
-      type: String,
-      required: true,
+    name: {
+        type: String,
+        required: [true, "Please enter your name"],
     },
-    url: {
-      type: String,
-      required: true,
+    email: {
+        type: String,
+        required: [true, "Please enter your email"],
+        unique: true,
+        validate: validator.isEmail,
     },
-  },
 
-  playlist: [
-    {
-      course: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Course",
-      },
-      poster: String,
+    password: {
+        type: String,
+        required: [true, "Please enter your password"],
+        minLength: [6, "Password must be at least 6 characters"],
+        select: false,
     },
-  ],
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+    },
 
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+    subscription: {
+        id: String,
+        status: {
+            type: String,
+            default: "active",
+        },
+    },
 
-  resetPasswordToken: String,
-  resetPasswordExpire: String,
+    avatar: {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        },
+    },
+
+    playlist: [
+        {
+            course: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Course",
+            },
+            poster: String,
+        },
+    ],
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+    resetPasswordToken: String,
+    resetPasswordExpire: String,
+
 });
+schema.pre('save', async function (next) {
+    if (!this.isModified('password'))
+        return next();
 
-schema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+})
 
 schema.methods.getJWTToken = function () {
-  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: "15d",
-  });
-};
+    return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: '15d',
+    })
+}
 
 schema.methods.comparePassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
+    return await bcrypt.compare(password, this.password);
+}
 
 schema.methods.getResetToken = function () {
-  const resetToken = crypto.randomBytes(20).toString("hex");
+    const resetToken = crypto.randomBytes(20).toString('hex');
 
-  this.resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
 
-  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+    this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
 
-  return resetToken;
-};
+    return resetToken;
+}
 
-export const User = mongoose.model("User", schema);
+export const User = mongoose.model('User', schema);
